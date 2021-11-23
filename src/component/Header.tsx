64b2da436b9e223d974900fd37aea12edfed82f9
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-23 17:58:20 +0300

    кнопка симуляции логина пользователя

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import { ReactComponent as Key } from '../assets/icons/key.svg'
+
+import { primary } from '../appTheme'
+
+import IconButton from './IconButton'
 import Navigation from './Navigation'
 
 import styled from 'styled-components'
@@ -12,10 +17,17 @@ justify-content: space-between;
 align-items: center;
 `;
 
+const StyledKey = styled(Key)`
+fill: currentColor;
+width: 28px;
+height: 28px;
+`;
+
 export default () => {
   return (
     <Header>
       <Navigation />
+      <IconButton type="button" theme={primary} icon={<StyledKey />} />
     </Header>
   )
 }
