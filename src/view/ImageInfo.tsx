922990fbebd8be479ffce2fe41922805676afa88
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-23 16:16:50 +0300

    кнопка возврата на главный экран

--- src/view/ImageInfo.tsx
+++ src/view/ImageInfo.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import {
   useParams,
+  useNavigate
 } from 'react-router-dom'
 
 import { ViewModel } from '../types/types';
@@ -9,6 +10,30 @@ import { ViewModel } from '../types/types';
 import NotFound from './NotFound';
 
 import ImageInfoCard from '../component/ImageInfoCard'
+
+import styled from 'styled-components'
+
+const BackButton = styled.button`
+width: 100%;
+display: flex;
+flex-direction: row;
+justify-content: center;
+
+font-family: arial;
+font-size: 1.1em;
+
+padding: 16px 24px;
+
+border: none;
+outline: none;
+border-radius: 4px;
+color: white;
+background-color: #5C6BC0;
+
+margin-top: 16px;
+
+cursor: pointer;
+`;
 
 type ImageInfoProps = {
   viewModel: ViewModel
@@ -18,6 +43,11 @@ export default ({
   viewModel
 }: ImageInfoProps) => {
   const params = useParams();
+  const navigate = useNavigate();
+
+  const handleGoBackClicked = useCallback(() => {
+    navigate('/');
+  }, [navigate]);
 
   const { getImage } = viewModel;
   const image = getImage(+params.id!);
@@ -29,6 +59,9 @@ export default ({
   }
 
   return (
-    <ImageInfoCard image={image} />
+    <>
+      <ImageInfoCard image={image} />
+      <BackButton type="button" onClick={handleGoBackClicked}>Go back</BackButton>
+    </>
   );
 }
