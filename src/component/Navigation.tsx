8c46a163f415d9d39424f723d45ddeded8a4a97b
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-23 15:17:17 +0300

    стили блока навигации

--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -4,13 +4,67 @@ import {
   NavLink
 } from 'react-router-dom'
 
+import styled from 'styled-components'
+
+const Navigation = styled.nav`
+color: black;
+
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: center;
+
+font-family: arial;
+font-size: 1.05em;
+line-height: 1.8em;
+`;
+
+const StyledLink = styled(NavLink)`
+display: block;
+color: black;
+text-decoration: none;
+padding: 4px 16px;
+width: 142px;
+
+border-radius: 4px;
+text-align: center;
+
+transition: box-shadow, 0.1s linear;
+
+&:active {
+  color: black;
+}
+
+&:visited {
+  color: black;
+}
+
+&.active {
+  transition: box-shadow, 0.1s linear;
+  box-shadow: 0 0 4px 2px rgba(0,0,0,0.2);
+
+  font-weight: 700;
+}
+`;
+
+type HorizontalSpacingProps = {
+  spacing?: number;
+}
+
+const HorizontalSpacing = styled.div`
+margin-right: ${({ spacing = 1 } : HorizontalSpacingProps) => `${spacing * 8}px`};
+`;
+
 export default () => {
   return (
-    <nav className="navigation">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/images/1">Image Info Page</NavLink>
-      <NavLink to="/sensitive-content">Private Page</NavLink>
-      <NavLink to="/not-found">404</NavLink>
-    </nav>
+    <Navigation>
+      <StyledLink to="/">Home</StyledLink>
+      <HorizontalSpacing />
+      <StyledLink to="/images/1">Image Info Page</StyledLink>
+      <HorizontalSpacing />
+      <StyledLink to="/sensitive-content">Private Page</StyledLink>
+      <HorizontalSpacing />
+      <StyledLink to="/not-found">404</StyledLink>
+    </Navigation>
   )
 }
