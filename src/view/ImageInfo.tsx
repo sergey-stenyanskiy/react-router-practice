46a8c2ace2b43118678ef28c3bddb26b0de9e6ce
Sergey Stenyanskiy <s.stenyansky@worksolutions.ru>
2021-11-23 13:27:53 +0300

    первоначальная логика экрана просмотра информации об изображении

--- src/view/ImageInfo.tsx
+++ src/view/ImageInfo.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-export default () => {
+import {
+  useParams,
+} from 'react-router-dom'
+
+import { ViewModel } from '../types/types';
+
+import NotFound from './NotFound';
+
+type ImageInfoProps = {
+  viewModel: ViewModel
+}
+
+export default ({
+  viewModel
+}: ImageInfoProps) => {
+  const params = useParams();
+
+  const { getImage } = viewModel;
+  const image = getImage(+params.id!);
+
+  if (!image) {
+    return (
+      <NotFound />
+    );
+  }
+
   return (
-    <>View Image</>
+    <>{image.url}</>
   );
 }
